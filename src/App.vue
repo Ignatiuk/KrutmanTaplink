14971d755ad7115e87760e7e62bd7ee74093f534
Alexander <54143543+merikovich@users.noreply.github.com>
2021-07-05 04:18:23 +0300

    init(*): version 1

---
+++ src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="taplink">
+    <div class="taplink__avatar-wrapper">
+      <img :src="require(`@/assets/img/ava.png`)" alt="Avatar" class="taplink__avatar">
+    </div>
+
+    <LinkButton
+      v-for="link in links"
+      :key="link.title"
+      v-bind="link"
+      class="taplink__link"
+    />
+  </div>
+</template>
+
+<script>
+import LinkButton from '@/components/LinkButton/LinkButton'
+
+export default {
+  name: 'App',
+
+  components: { LinkButton },
+
+  data() {
+    return {
+      links: [
+        {
+          title: 'Донатик',
+          description: 'Буду рад каждому донату 🙋‍♀️',
+          url: 'https://www.donationalerts.com/r/krutman',
+          icon: 'donation',
+        },
+
+        {
+          title: 'Telegram 💤',
+          description: 'Частые конкурсы 🌈',
+          url: 'https://t.me/brawl_stars_world',
+          icon: 'telegram',
+        },
+
+        {
+          title: 'Discord',
+          description: 'Общаюсь с подписотой ✊',
+          url: 'https://discord.com/invite/ucNAM3FZy9',
+          icon: 'discord',
+        },
+
+        {
+          title: 'Instagram 🎈',
+          description: 'За рекламой сюды 🤷‍♀️',
+          url: 'https://www.instagram.com/hyqqee/',
+          icon: 'instagram',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap');
+
+body,
+html {
+  font-family: 'Ubuntu', sans-serif;
+  margin: 0;
+  padding: 8px;
+}
+
+.taplink {
+  max-width: 800px;
+  margin: 0 auto;
+
+  &__avatar {
+    height: 100px;
+    margin: 0 auto;
+
+    &-wrapper {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      margin-bottom: 12px;
+    }
+  }
+
+  &__link:not(:last-child) {
+    margin-bottom: 16px;
+  }
+}
+</style>
---
+++ src/components/LinkButton/LinkButton.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="link-button" @click="onLinkClick">
+    <img class="link-button__icon" :src="require(`@/assets/img/icons/${icon}.svg`)" />
+
+    <div class="link-button__body">
+      <div class="link-button__title">{{ title }}</div>
+      <div class="link-button__description">{{ description }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LinkButton',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    icon: {
+      type: String,
+      required: true,
+    },
+
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    onLinkClick() {
+      window.open(this.url)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.link-button {
+  background-color: #FFC107;
+  color: white;
+  display: flex;
+  cursor: pointer;
+  margin: 0 auto;
+  padding: 16px;
+  border-radius: 16px;
+
+  &__icon {
+    height: 42px;
+    width: 42px;
+    margin-right: 12px;
+  }
+
+  &__body {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &__title {
+    font-weight: 600;
+  }
+}
+</style>
